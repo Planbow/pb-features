f54c6c4b98e844d3d95389f705a4c95c41ff3704
Nivaan <Nivaan@Nivaans-Mac.local>
2024-03-19 20:48:58 +0530

    some message

--- app/components/Clientsay/index.tsx
+++ app/components/Clientsay/index.tsx
@@ -8,7 +8,7 @@ const Clientsay = () => {
                 <h4 className="text-lg font-normal text-black text-center mt-4">Go fast with your ideation and validation,<br /> do more with Planbow</h4>
 
                 <div className="lg:relative">
-                    <Image src={'/assets/clientsay/startup.svg'} alt="avatar-image" width={1200} height={450} className="hidden lg:block" />
+                    <Image src={'/assets/clientsay/startup.jpeg'} alt="avatar-image" width={1200} height={450} className="hidden lg:block" />
                     <span className="lg:absolute lg:bottom-40 lg:left-80">
                         <div className="lg:inline-block bg-white rounded-2xl p-5 shadow-sm">
                         </div>
