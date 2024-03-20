420635af5845c8aa7e713dcd8b775911d246451e
Nivaan <Nivaan@Nivaans-Mac.local>
2024-03-20 11:03:04 +0530

    some message

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+
 import Banner from './components/Banner/Banner';
 import Companies from './components/Companies/Companies';
 import Buyers from './components/Buyers/index';
@@ -7,22 +8,20 @@ import Network from './components/Network/index';
 import Clientsay from './components/Clientsay/index';
 import Newsletter from './components/Newsletter/Newsletter';
 
-
-
 export default function Home() {
   return (
-<body> 
-  <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/21796752.js"></script>
-    <main>
-      <Banner />
-{/*      <Companies /> */}
-      <Provide />
-      <Buyers />
-      <Why />
-      <Network />
-      <Clientsay />
-      <Newsletter />
-    </main>
-    </body>
+    <div>
+      <main>
+        <Banner />
+        {/* <Companies /> */}
+        <Provide />
+        <Buyers />
+        <Why />
+        <Network />
+        <Clientsay />
+        <Newsletter />
+      </main>
+      <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/21796752.js"></script>
+    </div>
   )
 }
